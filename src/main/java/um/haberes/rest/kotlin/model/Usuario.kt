5b0ce7fc48bc5a1b349ec181868282fd969fde6e
dqmdz <daniel.quinterospinto@gmail.com>
2024-02-27 08:48:58 -0300

    Upgrade Spring Boot version

--- src/main/java/um/haberes/rest/kotlin/model/Usuario.kt
+++ src/main/java/um/haberes/rest/kotlin/model/Usuario.kt
@@ -15,7 +15,6 @@ data class Usuario(
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ", timezone = "UTC")
     var lastLog: OffsetDateTime? = null,
 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     var usuarioId: Long? = null
 
 ) : Auditable()
