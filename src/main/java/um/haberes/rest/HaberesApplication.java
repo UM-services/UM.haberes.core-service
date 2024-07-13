ac23c54970a5ad4fa0a316d0e2461e09d0ac1824
dqmdz <daniel.quinterospinto@gmail.com>
2024-07-13 14:27:59 -0300

    Deleting config server

--- src/main/java/um/haberes/rest/HaberesApplication.java
+++ src/main/java/um/haberes/rest/HaberesApplication.java
@@ -2,12 +2,8 @@ package um.haberes.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
 public class HaberesApplication {
 
 	public static void main(String[] args) {
--- src/main/java/um/haberes/rest/configuration/HaberesConfiguration.java
+++ src/main/java/um/haberes/rest/configuration/HaberesConfiguration.java
@@ -3,6 +3,8 @@
  */
 package um.haberes.rest.configuration;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -14,6 +16,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
+@EnableDiscoveryClient
+@EnableFeignClients
 @PropertySource("classpath:config/haberes.properties")
 public class HaberesConfiguration {
 
