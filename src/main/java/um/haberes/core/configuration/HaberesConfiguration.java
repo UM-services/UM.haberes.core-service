c81019caee221f78e579f9efa3a089f986be5828
dqmdz <daniel.quinterospinto@gmail.com>
2025-07-23 15:51:39 -0300

    Consul migration

--- src/main/java/um/haberes/core/configuration/HaberesConfiguration.java
+++ src/main/java/um/haberes/core/configuration/HaberesConfiguration.java
@@ -6,7 +6,6 @@ package um.haberes.core.configuration;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,7 +21,6 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableJpaAuditing
-@EnableDiscoveryClient
 @EnableFeignClients(basePackages = "um.haberes.core.client")
 @PropertySource("classpath:config/haberes.properties")
 public class HaberesConfiguration {
